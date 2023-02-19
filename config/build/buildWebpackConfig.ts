29ac1535fd733e729c284550bd27b249983fcafe
Albert <superalbertem@gmail.com>
2023-02-19 11:39:48 +0100

    Decomposing webpack config

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,25 @@
+import path from 'path';
+import webpack from 'webpack';
+
+import { buildLoaders } from './buildLoaders';
+import { buildPlugins } from './buildPlugins';
+import { buildResolvers } from './buildResolvers';
+import { BuildOptions } from './types/config';
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+  const { paths, mode } = options;
+  return {
+    mode,
+    entry: paths.entry,
+    module: {
+      rules: buildLoaders(),
+    },
+    resolve: buildResolvers(),
+    output: {
+      filename: '[name].[contenthash].js',
+      path: paths.build,
+      clean: true,
+    },
+    plugins: buildPlugins(paths),
+  };
+}
